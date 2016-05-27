f8ea90141877272b6821525de8f0507799870d95
Jackey07 <Jackey_BlueStar@hotmail.com>
2016-05-27 16:04:23 +0800

    reindent the file safe.h

--- src/safe.h
+++ src/safe.h
@@ -27,17 +27,17 @@
 #ifndef _SAFE_H_
 #define _SAFE_H_
 
-#include <stdarg.h> /* For va_list */
-#include <sys/types.h> /* For fork */
-#include <unistd.h> /* For fork */
+#include <stdarg.h> 		/* For va_list */
+#include <sys/types.h> 		/* For fork */
+#include <unistd.h> 		/* For fork */
 
 /** @brief Safe version of malloc
  */
-void * safe_malloc (size_t size);
+void *safe_malloc (size_t size);
 
 /* @brief Safe version of strdup
  */
-char * safe_strdup(const char *s);
+char *safe_strdup(const char *s);
 
 /* @brief Safe version of asprintf
  */
